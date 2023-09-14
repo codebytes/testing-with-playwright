d5983f7ace226b06695aa2bcea57e74e447bac2e
Chris Ayers <clayers@gmail.com>
2023-09-14 23:31:52 +0000

    updated test and config

--- tests/todoApp.spec.ts
+++ tests/todoApp.spec.ts
@@ -12,3 +12,13 @@ test('has 2 todos', async ({ page }) => {
   const todoItems = page.locator('li');
   await expect(todoItems).toHaveCount(2);
 });
+
+test('can add todos', async ({ page }) => {
+  await page.goto('/');
+  const todoItems = page.locator('li');
+  await expect(todoItems).toHaveCount(2);
+  await page.getByRole('textbox').fill('buy dog food');
+  await page.getByRole('button', { name: 'Add Todo' }).click();
+  const updatedTodoItems = page.locator('li');
+  await expect(updatedTodoItems).toHaveCount(3);
+});
